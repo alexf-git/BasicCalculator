c929307c0685c6c3c501fb051a15e4ca4bb159d2
fernandezalex.1423 <fernandezalex.1423@gmail.com>
2022-04-10 01:09:14 -0700

    adding files

---
+++ Operations.py
@@ -0,0 +1,16 @@
+class Operations():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self, a, b):
+        return a + b
+
+    def difference(self, a, b):
+        return a - b
+
+    def product(self, a, b):
+        return a * b
+
+    def quotient(self, a, b):
+        return a / b
